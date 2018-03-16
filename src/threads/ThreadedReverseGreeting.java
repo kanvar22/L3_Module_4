e079f278aeac0a6b83f614a12f6a35e3707d1a69
kanvar22 <league@WTS-iMac-11.attlocal.net>
2018-03-15 19:35:47 -0700

    Finished Module 5

--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,4 +1,4 @@
-
+package threads;
 public class ThreadedReverseGreeting {
   //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
   //Thread 2 creates Thread 3; and so on, up to Thread 50. 
@@ -6,11 +6,23 @@ public class ThreadedReverseGreeting {
   //but you should structure your program such that the threads print their greetings in reverse order.
 	
 	public static void main(String[] args) {
-		new Thread(()->newThread()).start();
+			new Thread(()->newThread(1)).start();
 	}
-	public static void newThread() {
-	for (int i = 0; i < 49; i++) {
-		System.out.println("Hello from Thread " + i);
+	public static void newThread(int j) {
+		if (j>50) {
+			
+		}
+		else {		
+		Thread t = new Thread(()->newThread(j + 1));
+		t.start();
+		
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Hello form Thread " + j);
 	}
 	}
 }
